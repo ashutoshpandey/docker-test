adce73628988fb6ff9102c514568b7716cb08513
Ashutosh Pandey <ashutosh@keriton.com>
2021-03-05 22:46:56 -0500

    Cleared duplicate git repository

---
+++ angularfront/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { map } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'angularfront';
+
+    data: any = '';
+
+    constructor(
+        private httpClient: HttpClient
+    ) {
+        this.getData();
+    }
+
+    async getData() {
+        let that = this;
+
+        this.httpClient.get('http://localhost:7777/get-data').pipe(map(this.extractData)).subscribe((result) => {
+            that.data = result;
+        });
+    }
+
+    /*
+     *  Extract data from response
+     */
+    private extractData(res: Response) {
+        let body = res;
+        return body || {};
+    }
+}
